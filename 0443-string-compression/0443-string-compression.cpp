feeab0b4edf18743c0048b8e481460247bf217c1
Abhinav Prakash Rai <79394301+RaiAbhinavPrakash@users.noreply.github.com>
2022-10-16 12:42:02 +0530

    Time: 16 ms (15.96%), Space: 9 MB (23.12%) - LeetHub

--- 0443-string-compression/0443-string-compression.cpp
+++ 0443-string-compression/0443-string-compression.cpp
@@ -12,14 +12,9 @@ public:
                 if(cnt > 1)
                 {
                     string s = to_string(cnt);
-                    // reverse(s.begin(),s.end());
-                    // cnt = stoi(s);
                     for(int x = 0; x<s.size(); x++)
                     {
-                        // int rem = cnt%10;
-                        // cnt /= 10;
                         k++;
-                        // chars[k] = '0'+rem;
                         chars[k] = s[x];
                     }
                 }
@@ -28,32 +23,23 @@ public:
                 cnt = 1;
             }
         }
+        
         chars[k] = chars[j];
-        // k++;
+        
         if(chars[chars.size()-1] == chars[j])
         {
             if(cnt == 1) k++;
             else if(cnt > 1)
             {
                 string s = to_string(cnt);
-                // reverse(s.begin(),s.end());
-                // cnt = stoi(s);
                 for(int x = 0; x<s.size(); x++)
                 {
-                    // int rem = cnt%10;
-                    // cnt /= 10;
-                    // cout<<rem<<"\n";
                     k++;
-                    // chars[k] = '0'+rem;
                     chars[k] = s[x];
                 }
                 k++;
             }
-            // cout<<chars[j]<<k;
-            // chars[k] = chars[j];
-            // j++;
         }
-        // cout<<j;
         return k;
     }
 };
